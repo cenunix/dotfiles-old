d545dbead0f0f4c9627f647e5fc483ad084d69ab
selmer443 <caden.hargrave@gmail.com>
2023-02-23 23:26:42 -0800

    tweaks for x13s

--- dot_config/eww/fool_moon/bar/scripts/executable_nvidia.sh
+++ dot_config/eww/fool_moon/bar/scripts/executable_nvidia.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
-nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits -l 2 2> /dev/null | while read -r line ; do
-[ "$result" = "Failed" ] && echo "NaN" || echo "$line%"
-done 
+# nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits -l 2 2> /dev/null | while read -r line ; do
+# [ "$result" = "Failed" ] && echo "NaN" || echo "$line%"
+# done 
+
+if nvidia-smi ; then
+  nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits -l 2 2> /dev/null | while read -r line ; do
+  [ "$result" = "Failed" ] && echo "NaN" || echo "$line%"
+  done 
+else 
+  echo "NaN"
+fi 
