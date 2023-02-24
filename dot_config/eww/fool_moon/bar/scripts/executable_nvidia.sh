f6f028bf0b600d33a7e0255ac6b27f2e7aa5dd8f
Selmer <caden.hargrave@gmail.com>
2023-02-23 17:07:41 -0800

    add more to bar

---
+++ dot_config/eww/fool_moon/bar/scripts/executable_nvidia.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits -l 2 2> /dev/null | while read -r line ; do
+[ "$result" = "Failed" ] && echo "NaN" || echo "$line%"
+done 
